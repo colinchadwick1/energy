840efe9103d676108e2c9b3e4dbfe79709062c9a
Colin Chadwick <colinchadwick@hotmail.co.uk>
2022-02-11 14:06:36 +0100

    icon

--- src/components/Icons.js
+++ src/components/Icons.js
@@ -1,4 +1,6 @@
 import contact from '../images/contacts.png';
+import clipboard from '../images/clipboard.png';
+import speaker from '../images/speaker.png'
 function Icons() {
     return ( 
         <div className="icons">
@@ -11,10 +13,14 @@ function Icons() {
             </div>
             <div>
             <div className="circle">
+            <img className="icon" src={clipboard} alt=""/>
+
             </div>
                 <p>TASK FORCE TASKS</p>
             </div> <div>
             <div className="circle">
+            <img className="icon" src={speaker} alt=""/>
+
             </div>
                 <p>TASK FORCE NEWS</p>
             </div>
